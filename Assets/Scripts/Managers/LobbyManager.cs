231796a0e4977e89346d566225c2c659879c3b1f
Ji_Won <121164034+NickJeongWib@users.noreply.github.com>
2023-09-23 05:40:41 +0900

    23_09_22
    -UI 추가

--- Assets/Scripts/Managers/LobbyManager.cs
+++ Assets/Scripts/Managers/LobbyManager.cs
@@ -8,13 +8,15 @@ namespace TowerDefence
     public class LobbyManager : MonoBehaviour
     {
         #region Var
-       
+
         #endregion
+        [SerializeField]
+        ScrollManager ScrollManagerRef;
 
         // Start is called before the first frame update
         void Start()
         {
-
+            
         }
 
         public void OnClickOpenPopUp_Btn(GameObject obj)
@@ -24,6 +26,18 @@ namespace TowerDefence
 
         public void OnClickClosePopUp_Btn(GameObject obj)
         {
+            // 던전선택화면의 닫기 버튼 클릭 시
+            if (obj.gameObject.name == "DungeonSelect_Close")
+            {
+                // 던전 선택화면의 스크롤 value를 0으로 초기화 해주고 오브젝트 비활성화.
+                obj.transform.parent.GetChild(1).GetComponent<Scrollbar>().value = 0;
+                obj.transform.parent.gameObject.SetActive(false);
+
+                ScrollManagerRef.targetPos = 0.0f;
+                ScrollManagerRef.curPos = 0.0f;
+                return;
+            }
+
             obj.transform.parent.gameObject.SetActive(false);
         }
     }
