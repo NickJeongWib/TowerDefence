75223d9614b7af5317eda31ea38290d6cdd7337a
Ji Won <jgw0929@gmail.com>
2023-09-24 08:34:30 +0900

    09_24_JGW
    - 웨이포인트마다 몬스터 방향 전환

--- Assets/Scripts/Enemy/EnemyMoveControl.cs
+++ Assets/Scripts/Enemy/EnemyMoveControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
 
 namespace TowerDefence
 {
@@ -10,8 +11,14 @@ namespace TowerDefence
         private float moveSpeed = 0.0f;
         [SerializeField]
         private Vector3 moveDirection = Vector3.zero;
-
         public float MoveSpeed => moveSpeed;   //moveSpeed 변수의 프로퍼티(Property) (Get 가능)
+        [SerializeField]
+        SkeletonAnimation skeletonAnimation;
+
+        void Start()
+        {
+            skeletonAnimation = GetComponent<SkeletonAnimation>();
+        }
 
         private void Update()
         {
@@ -23,5 +30,30 @@ namespace TowerDefence
             moveDirection = direction;
         }
 
+        // TODO ## EnemyMoveControl - 몬스터 충돌 관련
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            // JGW -- 웨이포인트 도착 시 몬스터 방향 전환
+            if(collision.CompareTag("Go_Right"))
+            {
+                skeletonAnimation.initialSkinName = "Side";
+                skeletonAnimation.AnimationName = "Side_Walk";
+                skeletonAnimation.Initialize(true);
+            }
+
+            if (collision.CompareTag("Go_Up"))
+            {
+                skeletonAnimation.initialSkinName = "Back";
+                skeletonAnimation.AnimationName = "Back_Walk";
+                skeletonAnimation.Initialize(true);
+            }
+
+            if (collision.CompareTag("Go_Front"))
+            {
+                skeletonAnimation.initialSkinName = "Front";
+                skeletonAnimation.AnimationName = "Front_Walk";
+                skeletonAnimation.Initialize(true);
+            }
+        }
     }
 }
