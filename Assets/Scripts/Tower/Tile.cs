fa72bdeaa6920236854e042257750c2a0ec85e12
Ji Won <jgw0929@gmail.com>
2023-10-22 18:03:25 +0900

    Revert "Merge branch 'main' into JGW"
    This reverts commit f613318c4b1aa6a26031fdf6091995b411cc9609, reversing
    changes made to 8472ee6254d4b2921c10050369c7aec02342a9ca.

--- Assets/Scripts/Tower/Tile.cs
+++ Assets/Scripts/Tower/Tile.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace TowerDefence
-{
+{ 
     public class Tile : MonoBehaviour
     {
-        public bool isOccupied = false; // 해당 타일이 사용 중인지 여부
+        public bool IsBuildTower { set; get; }
+
+        private void Awake()
+        {
+            IsBuildTower = false;
+        }
     }
 }
