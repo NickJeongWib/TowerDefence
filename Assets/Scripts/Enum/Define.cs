11fb39a1d54c4b12e36abc9585a7bca1e5207295
Ji Won <jgw0929@gmail.com>
2023-10-29 15:44:55 +0900

    2023_10_29_JGW
    1. 인벤토리 추가
    2. 캐릭터 상점 추가
    3. 게임 시작 버튼 수정

---
+++ Assets/Scripts/Enum/Define.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefence
+{
+    public class Define
+    {
+        public enum Stage_Level
+        {
+            Stage_1,
+            Stage_2,
+            Stage_3,
+            Stage_4,
+            Stage_5,
+            Stage_6,
+            Stage_7,
+            Stage_8,
+            Stage_9,
+            Stage_10,
+        }
+
+    }
+}
